4ea52b36b4a7bf0e6bc2fda488760ec5eb896088
Austin Gilbert <124075249+AustinGilbe@users.noreply.github.com>
2023-02-17 17:44:03 -0500

    Animations of playable Character

---
+++ 2D_Practice/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    private float dirX = 0f;
+    [SerializeField]private float moveSpeed = 7f;
+    [SerializeField]private float jumpForce = 14f;
+
+    private enum MovementState {idle, running, jumping, falling}
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        AnimationUpdate();
+    }
+       
+    private void AnimationUpdate()
+    {
+        MovementState state;
+
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else 
+        {
+            state = MovementState.idle;
+        }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        anim.SetInteger("state", (int)state);
+                
+    }
+}
