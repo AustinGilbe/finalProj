8e18cd166513fb8ba1ab0e9d5fc6259b44e8ff26
MariAuxiHer <13-10652@usb.ve>
2023-04-21 21:47:08 -0500

    add start screen

---
+++ 2D_Practice/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,17 @@
+/*
+    Script that adds functionality to the start button.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Create a public method so that the button can access it 
+    // When we click the start button, this method will be called and load the next scene? which is level one?????????????????????
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
